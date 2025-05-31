38bf5dbb180425c5e798652a653021ed75002e61
Apoorvi S <2k22.csds.2211248@gmail.com>
2025-05-31 20:04:48 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavBar from './components/NavBar.jsx'
 import HomePage from './pages/HomePage.jsx'
 import LoginPage from './pages/LoginPage.jsx'
@@ -6,7 +6,19 @@ import SignUpPage from './pages/SignUpPage.jsx'
 import SettingsPage from './pages/SettingsPage.jsx'
 import ProfilePage from './pages/ProfilePage.jsx'
 import {Routes, Route} from 'react-router-dom'
+import { useAuthStore } from './store/useAuthStore.js'
+import {Loader} from "lucide-react"
 const App = () => {
+  const {authUser, checkAuth, isCheckingAuth} = useAuthStore();
+  useEffect(()=>{
+    checkAuth();}, [checkAuth]);
+    console.log({authUser});
+  
+  if(isCheckingAuth && !authUser) return (
+    <div className = 'flex items-center kustify-center h-screen'>
+      <Loader className="size-10 animate-spin"/>
+    </div>
+  )
   return (
     <div>
       <NavBar/>
